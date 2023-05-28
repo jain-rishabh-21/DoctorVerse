a5450a60e5c62c78eb1f3b518bc338b1b6ab9534
naman1729 <namangautam000@gmail.com>
2023-05-28 16:50:33 +0530

    Improvements in doctor.js

--- server/model/doctor.js
+++ server/model/doctor.js
@@ -1,3 +1,4 @@
+const validator = require('validator')
 const mongoose=require('mongoose')
 
 const doctorSchema = new mongoose.Schema({
@@ -10,13 +11,68 @@ const doctorSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    /**Preferred language for effective patient-doctor communication and enhanced comprehension. */
+    Language: {
+        type: String,
+        required: true
+    },
+    /**
+     * Highlight your experience in implementing quality improvement initiatives, 
+     * adherence to evidence-based practices, and involvement in patient safety programs 
+     * to demonstrate your commitment to providing high-quality care.
+     */
+    qualityAssurance: {
+        type: String
+    },
+    emailAddress: {
+        type: String,
+        unique: true, 
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    // Time at which clinic will open
+    openTime: {
+        hours: {
+            type: Number, required: true, min: 0, max: 23
+        },
+        minutes: {
+            type: Number, required: true, min: 0, max: 59
+        },
+        seconds: {
+            type: Number, required: true, min: 0, max: 59
+        }
+    },
+    // TIme at which clinic will get closed
+    closeTime: {
+        hours: {
+            type: Number, required: true, min: 0, max: 23
+        },
+        minutes: {
+            type: Number, required: true, min: 0, max: 59
+        },
+        seconds: {
+            type: Number, required: true, min: 0, max: 59
+        }
+    },
+    clinicHygiene: {
+        type: Number, 
+        required:true,
+        min: 1,
+        max: 5
+    },
     education:{
         type:String
     },
     placeOfPractice:{
         type:String
     },
-    expirience:{
+    experience:{
         type:Number
     },
     reviews:[{
