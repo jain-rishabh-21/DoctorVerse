faf620f4cf677cf1fc3cbb760ac65144ecae89e3
Madhur Rastogi <rastogi.madhur87@gmail.com>
2023-06-03 16:57:05 +0530

    Add Joi Validation, Error Handling, JWT

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 
-require('./db/mongoose.js')
-const express=require('express')
-const app=express()
-const port=process.env.PORT || 4000
-app.use(express.json())
- 
+const connectDB = require('./db/mongoose.js')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const userRoute = require('./routes/user')
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const userRoute=require('./routes/user')
+//Connect DataBase
+connectDB();
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+dotenv.config({ path: '.env' })
+
+//Routes Call
 app.use(userRoute)
 
-
-app.listen(port,()=>{
-    console.log("Backend server is listening on port 4000")
+app.listen(port, () => {
+    console.log(`Backend server is listening on port ${port}`)
 })
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,29 +1,16 @@
-const mongoose=require('mongoose')
-const express=require('express')
-const User=require('../model/User')
-const router=new express.Router()
-
+const express = require('express')
+const router = express.Router()
+const { loginUser, registerUser, updateUser } = require('../controller/user');
+const { tokenVerify } = require('../utlits/jwt');
 
 //SIGNUP-ENDPOINT
-router.post('/userSignUp',async (req,res)=>{
-    const user=new User(req.body)
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }catch(e){
-        res.status(400).send('Something went Wrong!!')
-    }
-})
+router.post('/userSignUp', registerUser);
 
 
 //LOGIN-ENDPOINT
-router.post('/userLogin',async (req,res)=>{
-    try{
-        const user = await User.findByCredentials(req.body.email,req.body.password)
-        res.send(user) 
-    }catch(e){
-        res.status(400).send('Something went wrong!!')
-    }
-})
+router.post('/userLogin', loginUser);
 
-module.exports=router
+//UPDATE-ENDPOINT
+router.put('/userUpdate', tokenVerify, updateUser);
+
+module.exports = router
