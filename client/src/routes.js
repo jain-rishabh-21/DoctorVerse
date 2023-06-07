d2c86658104c5695e7770de0e52c0eefba0e0b2c
MuzmmilKhan1 <muzmmil.khan16@gmail.com>
2023-06-07 13:13:23 -0700

    Login Signup page added:wq :wq

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,12 +3,16 @@ import {  Route,  ReactLocation } from "react-location";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-
+import Login from "./pages/Login/Login";
 
 export const routes: Route[] = [
 
 {
         path:"/",
+        element:<Login/>
+},
+{
+        path:"/home",
         element:<Home/>
 
 },
@@ -19,7 +23,7 @@ export const routes: Route[] = [
 {
         path:"/about",
         element:<AboutUs/>
-}
+},
 
 
 
