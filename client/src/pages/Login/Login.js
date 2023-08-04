6c19828b51f6e2c05c27d9279353794cc97ce456
Rishu <rrishu561@gmail.com>
2023-08-04 12:53:39 +0530

    feat: improve login/signup form

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,62 +1,109 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './login.css'
+import { FaUserAlt } from 'react-icons/fa'
+import { MdEmail, MdPassword } from 'react-icons/md'
+import { AiFillEyeInvisible, AiFillEye, AiFillGoogleCircle, AiFillFacebook } from 'react-icons/ai'
+
 
 export default function Login() {
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  }
+
+  const showHidePassword = () => {
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+    if (showPassword) {
+      password.type = 'password';
+      confirmPassword.type = 'password';
+    } else {
+      password.type = 'text';
+      confirmPassword.type = 'text';
+    }
+  }
+
   return (
     <>
-    
-    <div className='rectangleRight'></div>
 
-    <div className='rectanglePurpleRight'></div>
+      <div className='rectangleRight'></div>
 
-    {/* Login Actual Div */}
-    <div className='loginDiv'>
-    <form className='formDiv' aria-label='login form'>
-      <h3>Doctor Verse</h3>
-      <div className='form-element'>
-        <label htmlFor='name'>Username</label>
-        <input name='name' className='input' type='text' autoComplete='off' id='name' required aria-required="true"></input>
-      </div>
-      <div className='form-element'>
-        <label htmlFor='email'>E-mail Address</label>
-        <input name='email' className='input' type='email' autoComplete='off' id='email' required aria-required="true"></input>
-      </div>
-      <div className='form-element'>
-        <label htmlFor='password'>Create Password</label>
-        <input name='password' className='input' type='password' autoComplete='off' id='password' required aria-required="true"></input>
-      </div>
-      <div className='form-element'>
-        <label htmlFor='confirmPassword'>Confirm Password</label>
-        <input name='confirm password' className='input' type='password' autoComplete='off' id='confirmPassword' required aria-required="true"></input>
-      </div>
-      {/* Form Buttons */}
-      <div className='FormButtonsDiv'>
-        <div className='signUp'><button className='formButton'>Sign Up</button></div>
-        <div className='line'></div>
-        <div className='signUpWithSocials'>
-          <div className='socialSignupHeading'>signup with</div>
-          <div className='signUpSocialsContainer'>
-          <a href="https://www.freepnglogos.com/pics/google-logo" title="Image from freepnglogos.com">
-                <img 
-                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png"
-                 width="50" height="50" alt="google logo icon png transparent background osteopathy" loading='lazy' />
-                </a>
-                <div className='dot'></div>
-                <a href="https://www.freepnglogos.com/pics/logo-facebookpng" title="Image from freepnglogos.com">
-                  <img style={{backgroundColor: "white",borderRadius: "50%",border: "1px solid rgb(0,0,0,0)"}} src="https://www.freepnglogos.com/uploads/facebook-logo-icon/facebook-logo-icon-facebook-logo-png-transparent-svg-vector-bie-supply-16.png"
-                   width="50" height="50" alt="facebook icon png background" loading='lazy' />
-                </a>
+      <div className='rectanglePurpleRight'></div>
+
+      {/* Login Actual Div */}
+      <div className='loginDiv'>
+        <form className='formDiv' aria-label='login form'>
+          <h3>Doctor Verse</h3>
+          <div className='form-element'>
+            <div className='flex gap-2 items-center justify-center text-center'>
+              <FaUserAlt className='relative items-center text-lg text-cyan-100' />
+              <label htmlFor='name' className='text-[1.2rem] text-cyan-100 text-center'>Username</label>
+            </div>
+            <input name='name' className='bg-transparent outline-none text-center text-white w-[200px] border-b-2 border-solid' type='text' autoComplete='off' id='name' required aria-required="true"></input>
           </div>
-        </div>
+
+          <div className='form-element'>
+            <div className='flex gap-2 items-center justify-center text-center'>
+              <MdEmail className='relative items-center text-lg text-cyan-100' />
+              <label htmlFor='email' className='text-[1.2rem] text-cyan-100 text-center'>E-mail Address</label>
+            </div>
+            <input name='email' className='bg-transparent outline-none text-center text-white w-[200px] border-b-2 border-solid' type='email' autoComplete='off' id='email' required aria-required="true"></input>
+          </div>
+
+          <div className='form-element'>
+            <div className='flex gap-2 items-center justify-center text-center'>
+              <MdPassword className='relative items-center text-lg text-cyan-100' />
+              <label htmlFor='password' className='text-[1.2rem] text-cyan-100 text-center'>Create Password</label>
+            </div>
+            <input name='password' className='bg-transparent outline-none text-center text-white w-[200px] border-b-2 border-solid' type='password' autoComplete='off' id='password' required aria-required="true"></input>
+          </div>
+
+          <div className='form-element'>
+            <div className='flex gap-2 justify-center items-baseline'>
+              <MdPassword className='relative items-center text-lg text-cyan-100' />
+              <label htmlFor='confirmPassword' className='text-[1.2rem] text-cyan-100 text-center'>Confirm Password</label>
+            </div>
+            <div className='relative items-center'>
+              <input name='confirm password' className='bg-transparent outline-none text-center text-white w-[200px] border-b-2 border-solid' type='password' autoComplete='off' id='confirmPassword' required aria-required="true"></input>
+              {
+                showPassword ? (
+                  <AiFillEyeInvisible className='relative bottom-6 left-[185px] items-center text-lg text-cyan-100' onClick={() => {
+                    togglePassword();
+                    showHidePassword();
+                  }} />
+                ) : (
+                  <AiFillEye className='relative bottom-6 left-[185px] items-center text-lg text-cyan-100' onClick={() => {
+                    togglePassword();
+                    showHidePassword();
+                  }} />
+                )
+              }
+            </div>
+          </div>
+
+          {/* Form Buttons */}
+          <div className='flex justify-around pb-3 items-center gap-6'>
+            <div className='signUp'><button className=''>Sign Up</button></div>
+            <div className='line'></div>
+            <div className='signUpWithSocials'>
+              <div className='socialSignupHeading'>signup with</div>
+              <div className='signUpSocialsContainer'>
+                <AiFillGoogleCircle className='text-5xl hover:text-slate-500 text-cyan-300 hover:scale-125 transition-all ease-in-out duration-150 cursor-pointer' />
+                <div className='dot'></div>
+                <AiFillFacebook className='text-5xl hover:text-[#0165E1] text-cyan-300 hover:scale-125 transition-all ease-in-out duration-150 cursor-pointer' />
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-    </div>
 
-    {/* Styling rectangles */}
-    {/*  */}
-    <div className='rectangleLeft'></div>
+      {/* Styling rectangles */}
+      {/*  */}
+      <div className='rectangleLeft'></div>
 
-    <div className='rectanglePurpleLeft'></div>
+      <div className='rectanglePurpleLeft'></div>
     </>
   )
 }
