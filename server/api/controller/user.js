059edc0064abe05d8b9faed27594ebefc3790d00
Deepanshu0703 <21bcs072@iiitdmj.ac.in>
2023-08-08 16:00:46 +0530

    backend: added captcha verification in signup

--- server/api/controller/user.js
+++ server/api/controller/user.js
@@ -2,20 +2,34 @@ const User = require('../models/user');
 const { encodePassword, decodePassword } = require('../Authentication/passwordEncodeDecode');
 const { userLoginSchema, userRegisterSchema, userUpdateSchema } = require('../validations/user');
 const { tokenSign } = require('../Authentication/jwt');
+const axios = require('axios');
 
 // SIGNUP-CONTROLLER
 const registerUser = async (req, res) => {
   try {
-    const payloadValidate = await userRegisterSchema.validateAsync(req.body);
-    const { name, email, password } = payloadValidate;
-    const hashPassword = await encodePassword(password);
-    const user = new User({
-      name,
-      email,
-      password: hashPassword
+    const payloadValidate = await userRegisterSchema.validateAsync({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
     });
-    await user.save();
-    res.status(201).json({ name, email });
+    const { name, email, password } = payloadValidate;
+    const secret = "YOUR SECRET KEY";
+    const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${req.body.Token}`;
+    const response = await axios.post(url);
+    if (response.data.success) {
+
+      const hashPassword = await encodePassword(password);
+      const user = new User({
+        name,
+        email,
+        password: hashPassword
+      });
+      await user.save();
+      res.status(201).json({ name, email });
+    }
+    else {
+      res.status(400).json({ message: 'Invalid Captcha' });
+    }
   } catch (e) {
     res.status(400).json({ message: e.message });
   }
